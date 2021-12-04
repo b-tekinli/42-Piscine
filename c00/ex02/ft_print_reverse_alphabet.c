395df48f98484ddcaab31f33c3bdd5be454db0eb
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2021-12-04 19:20:49 +0300

    Success: 85%
    Success: 85%

---
+++ c00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: btekinli <42istanbul.com.tr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/02 15:37:12 by btekinli          #+#    #+#             */
+/*   Updated: 2021/12/02 15:39:21 by btekinli         ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_reverse_alphabet(char reverse)
+{
+	reverse = 'z';
+	while (reverse >= 'a' )
+	{
+		ft_putchar(reverse);
+		reverse --;
+	}
+}
