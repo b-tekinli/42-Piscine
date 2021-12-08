a0b11ce574b964aba7a53f4fe4bedc473cafe346
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2021-12-08 12:26:54 +0300

    Success: 100%

--- c01/ex07/ft_rev_int_tab.c
+++ c01/ex07/ft_rev_int_tab.c
@@ -14,18 +14,18 @@
 
 void	ft_rev_int_tab(int *tab, int size)
 {
-        int		a;
-        int		b;
-        int	    swap;
+        int	a;
+        int	b;
+        int	swap;
 
         a = 0;
         b = size - 1;
         while (a < (size / 2))
         {
-                swap = tab[a];
-                tab[a] = tab[b];
-                tab[b] = swap;
-                a++;
-                b--;
+             swap = tab[a];
+             tab[a] = tab[b];
+             tab[b] = swap;
+             a++;
+             b--;
         }
 }
