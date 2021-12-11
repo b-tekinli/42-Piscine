1aab6424ebf1d2deab79c011f1f3e11e8a001e98
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2021-12-11 14:48:28 +0300

    Success: 85%

---
+++ c02/ex11/ft_putstr_non_printable.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putchar.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: btekinli <42istanbul.com.tr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/11 14:44:12 by btekinli          #+#    #+#             */
+/*   Updated: 2021/12/11 14:44:17 by btekinli         ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int	ft_char_printable(char c)
+{
+	if (c >= 32 && c <= 126)
+		return (1);
+	return (0);
+}
+
+void	ft_putstr_non_printable(char	*str)
+{
+	int				i;
+	unsigned char	a;
+
+	i = 0;
+	while (str[i])
+	{
+		a = str[i];
+		if (ft_char_printable(a))
+			ft_putchar(a);
+		else
+		{
+			ft_putchar('\\');
+			ft_putchar("0123456789abcdef"[a / 16]);
+			ft_putchar("0123456789abcdef"[a % 16]);
+		}
+		i++;
+	}
+}
+
+/*
+int		main(void)
+{
+	char	*string;
+	string = "Co\tcou\ntu va\200s bi\10en ?";
+	ft_putstr_non_printable(string);
+	return (0);
+}*/
