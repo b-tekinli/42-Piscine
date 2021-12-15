a0c43d88323597ce17b3c4d5f4effd4353e26b62
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2021-12-15 16:53:35 +0300

    Success: 100%

---
+++ C01/ex08/ft_sort_int_tab.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putchar.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: btekinli <42istanbul.com.tr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/07 23:23:15 by btekinli          #+#    #+#             */
+/*   Updated: 2021/12/07 23:23:20 by btekinli         ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int		counter;
+	int		swap;
+
+	counter = 0;
+	while (counter < size - 1)
+	{
+		if (tab[counter] > tab[counter + 1])
+		{
+			swap = tab[counter];
+			tab[counter] = tab[counter + 1];
+			tab[counter + 1] = swap;
+			counter = 0;
+		}
+		else
+			counter++;
+	}
+}
